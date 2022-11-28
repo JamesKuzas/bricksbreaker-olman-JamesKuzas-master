c66325d1bbba6881e188bcb9d4b65e0d0327ce02
James Kuzas <JRKuzas@student.fullsail.edu>
2022-11-27 20:58:23 -0600

    TODO# 4  The bricks now change colors again.

--- Game.cpp
+++ Game.cpp
@@ -31,7 +31,6 @@ void Game::Reset()
 	{
 		v.push_back(brick);
 		brick.x_position = brick.x_position + 15;
-		
 	}
 }
 
@@ -90,7 +89,7 @@ void Game::CheckCollision()
 	{
 		if (v[i].Contains(ball.x_position + ball.x_velocity, ball.y_position + ball.y_velocity))
 		{
-			brick.color = ConsoleColor(brick.color - 1);
+			v[i].color = ConsoleColor(brick.color - 1);
 			ball.y_velocity *= -1;
 
 			// TODO #5 - If the ball hits the same brick 3 times (color == black), remove it from the vector
